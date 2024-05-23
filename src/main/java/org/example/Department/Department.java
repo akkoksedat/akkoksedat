f2fea2083192da7f4f14faa76eaeed85cbaea4c9
akkoksedat <akkokksedat@gmail.com>
2024-05-23 13:38:49 +0300

    ik uygulaması yapıldı

---
+++ src/main/java/org/example/Department/Department.java
@@ -0,0 +1,22 @@
+package org.example.Department;
+
+public class Department {
+
+    public String name;
+    public String surname;
+    public String position;
+
+    public void  personelekle() {
+
+        this.name = name;
+        this.surname = surname;
+        this.position = position;
+        System.out.println("personel eklendi");
+    }
+
+    public void  personellistele() {
+        System.out.println("Personel: " + name + " " + surname + ", Pozisyon: " + position);
+
+    }
+
+}
---
+++ src/main/java/org/example/Person/Person.java
@@ -0,0 +1,29 @@
+package org.example.Person;
+
+import org.example.Department.Department;
+
+public class Person {
+        public static void main(String[] args) {
+                Department person1 = new Department();
+                Department person2 = new Department();
+
+                person1.name = "Sedat";
+                person1.surname = "Akkok";
+                person1.position = "qa engineer";
+
+                person2.name = "Oznur";
+                person2.surname = "Akkok";
+                person2.position = "Sales support";
+
+                person1.personelekle();
+                person1.personellistele();
+
+                person2.personelekle();
+                person2.personellistele();
+
+        }
+
+
+
+
+}
