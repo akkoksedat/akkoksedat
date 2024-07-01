fe1dd519af07b196fd8210c3b1fbbcaaef7a6566
akkoksedat <akkokksedat@gmail.com>
2024-07-01 23:23:14 +0300

    Ders 12 Odev 9 yapıldı

---
+++ src/main/java/org/example/Ders11/Person.java
@@ -0,0 +1,42 @@
+package org.example.Ders11;
+
+import java.time.LocalDate;
+
+public abstract class Person {
+    protected String ad;
+    protected String soyad;
+    protected LocalDate dogumTarihi;
+
+    public Person(String ad, String soyad, LocalDate dogumTarihi) {
+        this.ad = ad;
+        this.soyad = soyad;
+        this.dogumTarihi = dogumTarihi;
+    }
+
+    public abstract int emeklilikYasiniHesapla();
+
+    // Getter ve Setter metodları
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+
+    public LocalDate getDogumTarihi() {
+        return dogumTarihi;
+    }
+
+    public void setDogumTarihi(LocalDate dogumTarihi) {
+        this.dogumTarihi = dogumTarihi;
+    }
+}
